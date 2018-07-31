b1ff2421cad8049fae22922f7108ac15e30e4fae
Boat <kunlatidalawilai@gmail.com>
2018-07-31 15:13:14 +0700

    edit index page

--- blank_page.php
+++ blank_page.php
@@ -10,7 +10,239 @@
     <?php include_once('inc/navbar.php'); ?>
     <!-- END FOOTER -->
     <div class="wrapper">
-    	
+    	<!-- START SECTION GALLERY -->
+        <section class="section-gallery">
+            <div class="container">
+                <div class="top-title mx-auto">
+                    <img src="assets/images/Logo/logo.png" alt="Calypso logo" class="top-title-img">
+                </div>
+                <h2 class="text-top-title text-center">Template</h2>
+                
+                
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                        <label>
+                        Line height 
+                        </label>
+                        <div class="line-title line-title-h2 line-title-w300"></div>
+                        <div class="line-title line-title-h3 line-title-w300"></div>
+                        <div class="line-title line-title-h4 line-title-w300"></div>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <label>
+                        Line Width
+                        </label>
+                        <div class="line-title line-title-h2 line-title-w100"></div>
+                        <div class="line-title line-title-h2 line-title-w200"></div>
+                        <div class="line-title line-title-h2 line-title-w300"></div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                            <button type="submit" class="btn-cal btn-cal-sm">Submit</button>
+                            <button type="submit" class="btn-cal btn-cal-md">Submit</button>
+                            <button type="submit" class="btn-cal btn-cal-lg">Submit</button>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <div class="header-text-box">
+                            <label class="text-box-detail">About The Thai Dance Performance</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-4">
+                    <ul class="pagination justify-content-center">
+                      <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+        </section>
+            <!-- START SECTION HOT SHOW -->
+    <section class="section-hotshow">
+        <div class="container">
+            <div class="top-title mx-auto">
+                <img src="assets/images/Logo/logo.png" alt="Calypso logo" class="top-title-img">
+            </div>
+            <h2 class="text-top-title text-center">Hot Show</h2>
+            <div class="text-top-excerpt">
+                <label class="text-white text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </label>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-8">
+                    <div class="card-box">
+                        <div class="card-box-header">
+                            <h4 class="card-box-title">Traveler's Infarmation</h4>
+                        </div>
+                        <div class="card-box-content">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="card-box">
+                        <div class="card-box-header">
+                            <h4 class="card-box-title">Traveler's Infarmation</h4>
+                        </div>
+                        <div class="card-box-content">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </p>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- START SECTION NEWS -->
+    <section class="section-news">
+        <div class="container">
+            <div class="top-title mx-auto">
+                <img src="assets/images/Logo/logo.png" alt="Calypso logo" class="top-title-img">
+            </div>
+            <h2 class="text-top-title text-center">News</h2>
+            <div class="text-top-excerpt">
+                <label class="text-white text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </label>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="owl-carousel owl-theme owl-product" id="owl-news">
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/03.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO CABARET (SHOW ONLY)</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/05.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO THAI RESTAURANT CALYPSO CABARET</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/02.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO THAI RESTAURANT (DINNER ONLY)</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/03.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO CABARET (SHOW ONLY)</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/05.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO THAI RESTAURANT CALYPSO</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="sm-card">
+                                <div class="sm-card-img">
+                                    <img class="sm-card-img-top" src="assets/images/02.jpg" alt="Card image cap">
+                                </div>
+                                <div class="sm-card-body">
+                                    <label class="sm-card-text">CALYPSO THAI RESTAURANT (DINNER ONLY)</label>
+                                    <p class="sm-card-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                    <div class="w-100 text-center">
+                                        <a href="#" class="btn-cal btn-cal-sm">Submit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- START SECTION CONTACT -->
+    <section class="section-contact bg-bottom-linear">
+        <div class="container">
+            <div class="top-title mx-auto">
+                <img src="assets/images/Logo/logo.png" alt="Calypso logo" class="top-title-img">
+            </div>
+            <h2 class="text-top-title text-center">Contact us</h2>
+            <div class="contact-box">
+                <div class="row mx-0 mx-lg-4">
+                    <div class="col-12 col-md-6">
+                        <form>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput">Name</label>
+                            <input type="text" class="form-control cal-form" id="formGroupExampleInput" placeholder="Example input">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2">Email</label>
+                            <input type="text" class="form-control cal-form" id="formGroupExampleInput2" placeholder="Another input">
+                          </div>
+                          <div class="form-group">
+                            <label for="formGroupExampleInput2">Message</label>
+                            <input type="text" class="form-control cal-form" id="formGroupExampleInput2" placeholder="Another input">
+                          </div>
+                          <div class="cal-capcha">
+                            <img src="assets/images/newCaptchaAnchor.gif" alt="calypso" class="img-fluid">
+                          </div>
+                          <div class="w-100 my-4">
+                            <button type="submit" class="btn-cal btn-cal-md mr-4">Submit</button>
+                            <button type="submit" class="btn-cal btn-cal-md">reset</button>
+                          </div>
+                        </form>
+                    </div>
+                    <div class="col-12 col-md-6 text-center contact-fb-fanpage">
+                        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcalypsocabaret%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1469667919809348" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     </div>
 
     <!-- START FOOTER -->
@@ -19,6 +251,27 @@
 
     <!-- START FOOTER SCRIPT -->
     <?php include_once('inc/footer-script.php'); ?>
+    <script type="text/javascript">
+        $('#owl-news').owlCarousel({
+            loop:false,
+            margin:10,
+            nav:true,
+            navText:["<i class='far fa-arrow-alt-circle-left fa-2x'></i>","<i class='far fa-arrow-alt-circle-right fa-2x'></i>"],
+            dots:true,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:2
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
+    </script>
     <!-- END FOOOTER SCRIPT -->
 </body>
 
