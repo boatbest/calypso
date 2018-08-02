230b4733ac7558899fcd0a5a09ae4020bfaf4029
ilgenuine <infx@live.se>
2018-08-02 14:29:28 +0700

    update payment page

--- inc/footer-script.php
+++ inc/footer-script.php
@@ -4,4 +4,13 @@
     <script type="text/javascript" src="node_modules/popper.js/dist/umd/popper.min.js"></script>
     <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="assets/vendor/owlcarousel/js/owl.carousel.min.js"></script>
-    
+
+
+
+<script>
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+</script>
