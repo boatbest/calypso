3e74e11dced22392a2f540d0962a7acd87fd1f24
Boat <kunlatidalawilai@gmail.com>
2018-08-02 14:06:28 +0700

    edit -page index -page news_detail -css add -flaticon

--- news_detail.php
+++ news_detail.php
@@ -29,6 +29,9 @@
                          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                      </p>
                  </div>
+                 <div class="w-100 news-content-bottom text-center">
+                     <a href="news.php" class="btn-cal btn-cal-sm">Back</a>
+                 </div>
             </div>
         </section>
 
