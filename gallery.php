b5025c4cabe60fb494224dc411c83fcfc4bd3903
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-08-02 11:25:25 +0700

    add gallery.php 11.25 2/8/2561

--- gallery.php
+++ gallery.php
@@ -10,7 +10,165 @@
     <?php include_once('inc/navbar.php'); ?>
     <!-- END FOOTER -->
     <div class="wrapper">
-    	
+
+
+      <!-- HEAD -->
+      <div class="container">
+        <div class="top-title mx-auto my-3">
+            <img src="assets/images/Logo/logo.png" alt="Calypso logo" class="top-title-img">
+            <div class="form-title text-center mt-lg-5">
+              <h3>GALLERY</h3>
+            </div>
+        </div>
+        <div class="col-md-6 col-12 mx-auto">
+          <div class="text-center mt-lg-5 mt-3">
+            <p>Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry'sd</p>
+          </div>
+        </div>
+      </div>
+      <!-- END HEAD -->
+      <!-- <div id="aniimated-thumbnials">
+        <a href="img/img1.jpg">
+          <img src="img/thumb1.jpg" />
+        </a>
+        <a href="img/img2.jpg">
+          <img src="img/thumb2.jpg" />
+        </a>
+      </div> -->
+
+
+
+
+      <section>
+        <div class="container">
+          <div class="row" id="aniimated-thumbnials">
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/about/CAL_7219.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/about/CAL_7219.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/01.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/01.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/02.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/02.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/03.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/03.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/04.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/04.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/05.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/05.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/01.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/01.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/02.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/02.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12 my-3" href="assets/images/about/GIGI3637.jpg">
+              <div class="galley-show">
+                  <div class="galley-fix">
+                      <img src="assets/images/about/GIGI3637.jpg" class="ab-cal-image">
+                  </div>
+                  <div class="galley-overlay">
+                    <div class="galley-text">
+                        <i class="fa fa-search-plus" aria-hidden="true"></i>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <div class="container">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-center">
+            <li class="page-item"><a class="galley-page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="galley-page-link" href="#">1</a></li>
+            <li class="page-item"><a class="galley-page-link galley-page-link-active" href="#">2</a></li>
+            <li class="page-item"><a class="galley-page-link" href="#">3</a></li>
+            <li class="page-item"><a class="galley-page-link" href="#">Next</a></li>
+          </ul>
+        </nav>
+      </div>
+
+
     </div>
 
     <!-- START FOOTER -->
@@ -23,3 +181,12 @@
 </body>
 
 </html>
+
+
+<script type="text/javascript">
+  $('#aniimated-thumbnials').lightGallery({
+    thumbnail:true,
+    animateThumb: false,
+    showThumbByDefault: false,
+  });
+</script>
--- inc/footer-script.php
+++ inc/footer-script.php
@@ -4,4 +4,9 @@
     <script type="text/javascript" src="node_modules/popper.js/dist/umd/popper.min.js"></script>
     <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="assets/vendor/owlcarousel/js/owl.carousel.min.js"></script>
-    
+
+    <!-- Plug-in scrip -->
+    <script src="https://cdn.jsdelivr.net/picturefill/2.3.1/picturefill.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="assets/vendor/lightgallery/dist/js/lightgallery-all.min.js"></script>
+    <script src="assets/vendor/lightgallery/lib/jquery.mousewheel.min.js"></script>
